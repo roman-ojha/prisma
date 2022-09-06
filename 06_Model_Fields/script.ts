7f55d8f88d35879bd9d96129d06dc4123600c910
Roman-Ojha <razzroman98@gmail.com>
2022-09-06 16:26:50 +0545

    docs: Model Fields

---
+++ 06_Model_Fields/script.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const createdUser = await prisma.user.create({ data: { name: "Roman" } });
+  console.log(createdUser);
+
+  const user = await prisma.user.findMany();
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
